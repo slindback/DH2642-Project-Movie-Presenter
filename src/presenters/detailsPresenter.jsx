2933cb377d38ab64fb3d4978e1c27f0fc6b2c474
Simon Lindbäck <slindbac@kth.se>
2023-12-06 15:31:30 +0100

    Details page layout created. Err: Fetching fails.

--- src/presenters/detailsPresenter.jsx
+++ src/presenters/detailsPresenter.jsx
@@ -1,9 +1,24 @@
 import DetailsView from "/src/views/detailsView.jsx";
 
-
 export default
 function Details(props) {
+  if (!props.model.currentMoviePromiseState.promise) {
+    return "No data...";
+  }
+
+  if (props.model.currentMoviePromiseState.error) {
+    return props.model.currentMoviePromiseState.error;
+  }
+
+  if (props.model.currentMoviePromiseState.data) {
+    return <DetailsView
+      movieDetails={props.model.currentMoviePromiseState.data}
+    />;
+  }
+
   return (
-    <DetailsView/>
+    <img className="loadingImage"
+      src = "https://www.elevateyourwellness.org/Content/image/loader2.gif"
+    />
   );
 }
