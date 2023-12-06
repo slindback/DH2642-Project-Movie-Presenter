c0bd5bdb679bf9eb19ae785d5909832ae3c423f7
Simon Lindbäck <slindbac@kth.se>
2023-12-06 17:03:19 +0100

    Fixed detailsView

--- src/views/detailsView.jsx
+++ src/views/detailsView.jsx
@@ -2,7 +2,7 @@ import "/src/style.css";
 
 export default
 function DetailsView(props) {
-  console.log(">",props.movieDetails);
+  console.log(">",props.movieDetails.genres);
   return (
     <div className="detailsContainer">
       <div className="titleHeader">
@@ -11,22 +11,22 @@ function DetailsView(props) {
           <p>{props.movieDetails.original_title || 'Original Title'}</p>
         </div>
         <div className="rating">
-          <p>{props.movieDetails.vote_average ? `${props.movieDetails.vote_average}/10` : '?/10'}</p>
-          <p>{props.movieDetails.vote_count ? `${props.movieDetails.vote_count} votes` : '? votes'}</p>
+          <p>{props.movieDetails.vote_average.toFixed(1)+"/10" || "?/10"}</p>
+          <p>{props.movieDetails.vote_count + " votes" || '? votes'}</p>
         </div>
       </div>
       <div className="year">
-        <p>{props.movieDetails.release_date ? props.movieDetails.release_date.substring(0, 4) : 'Year not available'}</p>
+        <p>{props.movieDetails.release_date.substring(0, 4) || 'Year not available'}</p>
       </div>
       <div className="pictures">
         <img className="moviePoster" src={"https://image.tmdb.org/t/p/w300"+props.movieDetails.poster_path} alt={`${props.movieDetails.title} poster`} />
         <img className="movieBackdrop" src={"https://image.tmdb.org/t/p/original"+props.movieDetails.backdrop_path} alt={`${props.movieDetails.title} backdrop`} />
       </div>
       <div className="genres">
-        <p>{props.movieDetails.genres ? props.movieDetails.genres.join(', ') : 'Genres not available'}</p>
+        <p>{props.movieDetails.genres.map(genre => genre.name).join(', ') || 'Genres not available'}</p>
       </div>
       <div className="genre">
-        <p>{props.movieDetails.overview ? `${props.movieDetails.overview}` : 'Movie description not available'}</p>
+        <p>{props.movieDetails.overview || 'Movie description not available'}</p>
       </div>
     </div>
   );
