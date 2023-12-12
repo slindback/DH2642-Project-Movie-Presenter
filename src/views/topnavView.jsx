5d50c69c9b04a1b0217f5b2f554e5e8725fa7de5
Yirkarja <unerater@gmail.com>
2023-12-13 00:52:41 +0100

    removed a wrongly placed "¤"

--- src/views/topnavView.jsx
+++ src/views/topnavView.jsx
@@ -6,11 +6,11 @@ export function TopnavView(props) {
     return (
       <div>
         <div className="topnavSubcontainer topnavSubcontainerLeft">
-          <a className="topnavLink" href="¤">WatchLater</a>
+          <a className="topnavLink" href="">WatchLater</a>
         </div>
         <div className="topnavSubcontainer topnavSubcontainerRight">
-          <a className="topnavLink" href="¤">Place Holder 1</a>
-          <a className="topnavLink" href="¤">Place Holder 2</a>
+          <a className="topnavLink" href="">Place Holder 1</a>
+          <a className="topnavLink" href="">Place Holder 2</a>
         </div>
       </div>
     );
